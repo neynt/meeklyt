ffc6d23e3139d6f75df5cc45adab51a0912a23bf
Jim Zhang <Hyriodula@gmail.com>
2014-08-13 12:51:22 -0400

    remove ads, and add the splitted files

--- web.js
+++ web.js
@@ -1,5 +1,4 @@
 // Require stuff
-var request = require('request');
 var express = require('express');
 var bodyParser = require('body-parser');
 var logfmt = require('logfmt');
@@ -14,51 +13,11 @@ db.once('open', function () {
   // yay!
 });
 
-// Database models
-var VideoSchema = mongoose.Schema({
-  _id: String,
-  title: String,
-  pure_likes: Number,
-  likes: Number,
-  dislikes: Number
-});
-
-var Video = mongoose.model('Video', VideoSchema);
+// Models
+var Video = require('./models/video');
 
 // Functions
-function update_video(vid) {
-  var baseurl = 'https://www.googleapis.com/youtube/v3';
-  var url = baseurl + '/videos';
-  url += '?part=snippet,statistics,status';
-  url += '&id=' + vid;
-  url += '&key=' + process.env.GOOGLE_API_KEY;
-
-  console.log("muh url is " + url);
-
-  request(url, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-      var stuff = JSON.parse(body);
-
-      if (stuff.items.length > 0) {
-        var v = stuff.items[0];
-        var cur_vid = new Video();
-        cur_vid._id = vid;
-        cur_vid.title = v.snippet.title;
-        cur_vid.likes = v.statistics.likeCount;
-        cur_vid.dislikes = v.statistics.dislikeCount;
-        if (cur_vid.dislikes == 0) {
-          cur_vid.pure_likes = cur_vid.likes;
-        }
-        cur_vid.save(function (err, video) {
-          if (err) return console.error(err);
-        });
-
-        console.log("jitsu wa, i really like " + cur_vid.likes);
-      }
-    }
-  });
-}
+var update_video = require('./update_video');
 
 // Middleware
 var app = express();
